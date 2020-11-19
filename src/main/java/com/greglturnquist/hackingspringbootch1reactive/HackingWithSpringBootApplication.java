14fc7abfd2947ea91a383ce54063910864a5e6a8
Andrew <andrewriznykua93@gmail.com>
2020-11-18 23:27:45 -0600

    Chapter 1 completed

---
+++ src/main/java/com/greglturnquist/hackingspringbootch1reactive/HackingWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.greglturnquist.hackingspringbootch1reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackingWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackingWithSpringBootApplication.class, args);
+	}
+
+}
